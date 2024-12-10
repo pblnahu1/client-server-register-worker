8ee5288217a8183cbb6393475cc31ccfebea0562
pblnahu1 <pablotorrez90@outlook.com>
2024-12-10 14:46:57 -0300

    updating files

--- backend/api/access-control.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// Configuro CORS para que Angular haga peticiones al servidor PHP desde el dominio que le paso
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
-header("Access-Control-Allow-Headers: Content-Type");
-
-?>
